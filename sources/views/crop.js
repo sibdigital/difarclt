980a5e2c5cc040b109cb8c8e9f1ad25e9e42ddba
ts808 <tmd979@gmail.com>
2020-02-07 16:41:34 +0800

    Создание crud-интерфейса для справочника (crop)

--- sources/views/crop.js
+++ sources/views/crop.js
@@ -3,29 +3,38 @@ import { JetView } from "webix-jet";
 export default class DataView extends JetView {
 	config() {
 		return {
-			width: 400,
 			rows: [
 				{
 					view: "form", id: "cropForm", elements: [
-						{ view: "text", placeholder: "Name", id: "nameInput" },
-						{ view: "text", placeholder: "Code", id: "codeInput" },
-						{ view: "text", placeholder: "Number", id: "numberInput" }
+						{ view: "text", placeholder: "Name", id: "name" },
+						{ view: "text", placeholder: "Code", id: "code" },
+						{ view: "text", placeholder: "Number", id: "number" }
 					]
 				},
 				{
 					view: "toolbar", elements: [
 						{ view: "button", value: "Save", width: 100, click: () => this.saveRow() },
-						{ view: "button", value: "Delete", width: 100, click: () => this.deleteRow() }
+						{ view: "button", value: "Delete", width: 100, click: () => this.deleteRow() },
+						{ view: "button", value: "Update", width: 100, click: () => this.updateRow() }
 					]
 				},
 				{
 					view: "datatable",
 					id: "cropTable",
+					on: {
+						onAfterSelect: (id) => {
+							const item = $$("cropTable").getItem(id);
+
+							$$("name").setValue(item.name);
+							$$("number").setValue(item.number);
+							$$("code").setValue(item.code);
+						},
+					},
 					select: true, //enables selection 
 					columns: [
-						{ id: "name", header: "Name", width: "100" },
-						{ id: "code", header: "Code", width: "100" },
-						{ id: "number", header: "Number", width: "100" }
+						{ id: "name", header: "Name", width: "140" },
+						{ id: "code", header: "Code", width: "140" },
+						{ id: "number", header: "Number", width: "140" }
 					],
 				}
 			]
@@ -37,23 +46,54 @@ export default class DataView extends JetView {
 	}
 
 	loadData() {
-		this.$$("cropTable").load(() => {
-			return this.webix.ajax().get("http://localhost:8080/crop");
+		$$("cropTable").load(() => {
+			return webix.ajax().get("http://localhost:8080/crop");
 		});
 	}
 
 	deleteRow() {
-		const id = this.$$("cropTable").getSelectedId();
-		this.webix.ajax().del("http://localhost:8080/crop/" + id).then(() => this.loadData());
+		const id = $$("cropTable").getSelectedId();
+
+		webix.ajax().del("http://localhost:8080/crop/" + id)
+			.then(data => {
+				if (data.text()) {
+					$$("cropTable").remove(id);
+				}
+			});
 	}
 
 	saveRow() {
-		const name = this.$$("nameInput").getValue();
-		const code = this.$$("codeInput").getValue();
-		const number = this.$$("numberInput").getValue();
+		const item = {
+			name: $$("name").getValue(),
+			code: $$("code").getValue(),
+			number: $$("number").getValue()
+		};
 
-		this.webix.ajax().headers({
+		webix.ajax().headers({
 			"Content-Type": "application/json"
-		}).post("http://localhost:8080/crop/create", { name: name, code: code, number: number }).then(() => this.loadData());
+		}).post("http://localhost:8080/crop/create", item)
+			.then(data => {
+				if (data.json() != null) {
+					$$("cropTable").add(data.json());
+				}
+			});
+	}
+
+	updateRow() {
+		const id = $$("cropTable").getSelectedId();
+		const item = $$("cropTable").getItem(id);
+
+		item.name = $$("name").getValue();
+		item.code = $$("code").getValue();
+		item.number = $$("number").getValue();
+
+		webix.ajax().headers({
+			"Content-Type": "application/json"
+		}).put("http://localhost:8080/crop/update", item)
+			.then(data => {
+				if (data.json() != null) {
+					$$("cropTable").updateItem(id, data.json());
+				}
+			});
 	}
 }
